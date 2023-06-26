b7b58edffb6799642ccfe52fe636f64dda059862
Alfaz <alfazkherani@gmail.com>
2023-06-26 11:46:01 +0530

    Recycler adapter

--- app/src/main/java/com/example/myfamily/HomeFragment.kt
+++ app/src/main/java/com/example/myfamily/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 
 class HomeFragment : Fragment() {
@@ -20,6 +22,22 @@ class HomeFragment : Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val listMembers = listOf<MemberModel>(
+            MemberModel("Alfaz Kherani"),
+            MemberModel("Aliyan Kherani"),
+            MemberModel("Rupa Kherani"),
+            MemberModel("Asif Kherani")
+        )
+        val adapter = MemberAdapter(listMembers)
+
+        val recycler = requireView().findViewById<RecyclerView>(R.id.recyler_view_member)
+        recycler.layoutManager = LinearLayoutManager(requireContext())
+        recycler.adapter = adapter
     }
 
     companion object {
--- app/src/main/java/com/example/myfamily/MainActivity.kt
+++ app/src/main/java/com/example/myfamily/MainActivity.kt
@@ -14,16 +14,25 @@ class MainActivity : AppCompatActivity() {
 
         bottomBar.setOnItemSelectedListener {
 
-            if(it.itemId == R.id.nav_guard){
-                inflateFragment(GuardFragment.newInstance())
-            }else if(it.itemId == R.id.nav_home){
-                inflateFragment(HomeFragment.newInstance())
+            when (it.itemId) {
+                R.id.nav_guard -> {
+                    inflateFragment(GuardFragment.newInstance())
+                }
+                R.id.nav_home -> {
+                    inflateFragment(HomeFragment.newInstance())
+                }
+                R.id.nav_dashboard -> {
+                    inflateFragment(DashboardFragment.newInstance())
+                }
+                R.id.nav_profile -> {
+                    inflateFragment(ProfileFragment.newInstance())
+                }
             }
-
 
 
             true
         }
+        bottomBar.selectedItemId = R.id.nav_home
     }
 
 
